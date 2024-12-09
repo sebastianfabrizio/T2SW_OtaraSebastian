2825d60685343cea41508beb32f0626c8bff9956
sebastianfabrizio <sebasotaradiaz@gmail.com>
2024-12-08 20:59:43 -0500

    Creando método guardar especialidad

--- src/main/java/pe/com/cibertec/t2sw_otarasebastian/controller/EspecialidadController.java
+++ src/main/java/pe/com/cibertec/t2sw_otarasebastian/controller/EspecialidadController.java
@@ -4,10 +4,7 @@ package pe.com.cibertec.t2sw_otarasebastian.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pe.com.cibertec.t2sw_otarasebastian.dto.EspecialidadDto;
 import pe.com.cibertec.t2sw_otarasebastian.exception.ResourceNotFoundException;
 import pe.com.cibertec.t2sw_otarasebastian.service.IEspecialidadService;
@@ -31,6 +28,14 @@ public class EspecialidadController {
 
     @GetMapping("/{id}")
         public ResponseEntity<EspecialidadDto> ObtenerIdEspecialidad(@PathVariable Integer id) {
+        EspecialidadDto valorId = especialidadService.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("La especialidad con el identificador número " + id + " no existe"));
+        return new ResponseEntity<>(valorId, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<EspecialidadDto> GuardarEspecialidad(@RequestBody EspecialidadDto especialidadDto){
 
     }
+
 }
